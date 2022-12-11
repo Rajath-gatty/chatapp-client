e31f271fb0e96f5d3f10978cdacf6f70856eb1a2
Rajath-gatty <rajathgatty253@gmail.com>
2022-12-11 10:47:30 +0530

    url changed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 import App from "./App";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://chatapp-backend-01.herokuapp.com";
+axios.defaults.baseURL = "https://chatapp-backend-kd8k.onrender.com";
 // axios.defaults.baseURL = "http://localhost:8080";
 
 ReactDOM.render(
